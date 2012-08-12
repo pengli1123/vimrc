5097513bed917096370d0eccd7e897fd6a1c8ae5
Peng Li <pengli1123@gmail.com>
2012-08-12 13:25:20 -0700

    change setup.bat to use vimpath, bundle, and homedir instead of direct copy to program file directory

--- setup.bat
+++ setup.bat
@@ -1,31 +1,14 @@
 
 @echo off
 
-if "%~1"=="" (
-    echo "Input vim installer program file. i.e. ""C:\Program Files (x86)\Vim\vim73"" in quotes "
-    goto :eof
-)
+mkdir %HOMEPATH%\vimfiles
+mkdir %HOMEPATH%\vimfiles\autoload
+mkdir %HOMEPATH%\vimfiles\bundle
 
-echo %1%
+copy .\submodules\vim-pathogen\autoload\pathogen.vim %HOMEPATH%\vimfiles\autoload\
 
-set tst="%windir%\$del_me$"
 
-REM test if running in elevated mode
-(type nul>%tst%) 2>nul && (del %tst% & set elev=t) || (set elev=)
+xcopy /Y /S .\submodules %HOMEPATH%\vimfiles\bundle\
 
-if not defined elev ( 
-echo "ERROR, This script is required to be run in elevated mode!"
-goto :eof
-)
+copy .\vimrc %HOMEPATH%\_vimrc
 
-echo "copying tagbar files ..."
-
-copy .\submodules\tagbar\autoload\tagbar.vim %1%\autoload\
-copy .\submodules\tagbar\plugin\tagbar.vim %1%\plugin\
-copy .\submodules\tagbar\syntax\tagbar.vim %1%\syntax\
-
-copy .\submodules\tagbar\doc\tagbar.txt %1%\doc\
-
-copy .\imports\ctags58\ctags.exe %windir%\
-
-copy .\vimrc %1%\
