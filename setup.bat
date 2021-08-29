ee1785ee9536b15f1ea8532e130bb10cec82aa1c
Peng Li <pengli1123@gmail.com>
2021-08-29 01:40:23 -0700

    remove stuff

--- setup.bat
+++ setup.bat
@@ -1,14 +1,7 @@
 
 @echo off
 
-mkdir %HOMEPATH%\vimfiles
-mkdir %HOMEPATH%\vimfiles\autoload
-mkdir %HOMEPATH%\vimfiles\bundle
 
-copy .\submodules\vim-pathogen\autoload\pathogen.vim %HOMEPATH%\vimfiles\autoload\
-
-
-xcopy /Y /S .\submodules %HOMEPATH%\vimfiles\bundle\
 
 copy .\vimrc %HOMEPATH%\_vimrc
 
--- setup.sh
+++ setup.sh
@@ -1,9 +1,4 @@
 #!/bin/sh
 
-mkdir -p ~/.vim/autoload
-mkdir -p ~/.vim/bundle
-
-cp -r -f ./submodules/vim-pathogen/autoload/pathogen.vim ~/.vim/autoload/
-cp -r -f ./submodules/* ~/.vim/bundle/
 cp -r -f ./vimrc ~/.vimrc
 
